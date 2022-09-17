b962c277c5dbfbf2d66bd96e074ff2082b0b18d2
Muhammad Akita Jabbar Razzaq <majabbarrazzaq@gmail.com>
2022-09-17 09:12:18 +0700

    view product

---
+++ resources/views/product/index.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Produk | {{ $title }}</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+
+<body style="background: lightgray">
+
+    <div class="container mt-5">
+        <center>
+            <h3> Tampilan Produk</h3>
+        </center>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card border-0 shadow rounded">
+                    <div class="card-body">
+                        <a href="{{ route('product.create') }}" class="btn btn-md btn-danger mb-3">Add Produk</a>
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Nama Produk</th>
+                                    <th scope="col">Gambar Produk</th>
+                                    <th scope="col">Deskripsi Produk</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($products as $product)
+                                <tr>
+                                    <td>{{ $product->namaproduk }}</td>
+                                    <td class="text-center">
+                                        <img src="{{ Storage::url('public/products/').$product->gambar }}" class="rounded" style="width: 150px">
+                                    </td>
+                                    <td>{{ $product->deskripsi }}</td>
+                                </tr>
+                                @empty
+                                <div class="alert alert-danger">
+                                    Data product belum Tersedia.
+                                </div>
+                                @endforelse
+                            </tbody>
+                        </table>
+                        {{ $products->links() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+    <script>
+        //message with toastr
+    </script>
